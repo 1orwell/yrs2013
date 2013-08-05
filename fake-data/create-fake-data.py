c08faaf6b4cc118db38e3b8bbd27e1e6e1f97e63
Edward Bennigsen <pe.ads0@gmail.com>
2013-08-05 18:50:23 +0100

    Finished random generation code

--- fake-data/create-fake-data.py
+++ fake-data/create-fake-data.py
@@ -1,14 +1,50 @@
 #This script creates fake nodes and interactions to test virus spread on
 
-
+import random
 
 nodes = 10
 
-for currentnode in range(1,nodes+1):
-    currentfilename = "node-" + str(currentnode) + ".txt"
-    currentfile =  file("moteFiles/" + currentfilename,"a")
-    currentfile.write("This is node" + str(currentnode) + "\n")
-    currentfile.close()
 
 
-print "Finito"
+#move through time
+for tick in range(0,30):
+    print "Tick is ", tick
+    interactions = {}
+    for node in range(1,(nodes+1)):
+        interactions[node] = []
+    print interactions
+    #look at nodes in turn
+    for nodeA in range(1,(nodes+1)):
+        print "Node A is " + str(nodeA)
+        for nodeB in range(1,(nodes+1)):
+            #see if node interacts
+            interaction = random.randint(0,4)
+            #if node interacts
+            if interaction == 1:
+
+                if nodeB not in interactions[nodeA]:
+                    #mark node as interacted with for this tick
+                    interactions[nodeA].append(nodeB)
+                    interactions[nodeB].append(nodeA)
+
+                    #create/access files 
+                    fileAname = "node-" + str(nodeA) + ".txt"
+                    fileA =  file("moteFiles/" + fileAname,"a")
+                    fileBname = "node-" + str(nodeB) + ".txt"
+                    fileB =  file("moteFiles/" + fileBname,"a")
+                    
+
+                    #generate random sequence and strength
+                    sequence = str(random.randint(1,nodes))
+                    strength = "230"
+                    time = str(tick)
+                    #write interaction to files
+                    fileA.write(str(nodeB) + " " + sequence + " " + strength + " " + sequence + " " + time + "\n")
+                    fileB.write(str(nodeA) + " " + sequence + " " + strength + " " + sequence + " " + time + "\n")
+                    
+                
+            
+                    fileA.close()
+                    fileB.close()
+
+    
