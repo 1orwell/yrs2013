88a849b981ed821392229498f87eb124f24795cf
1orwell <elise@elise-Aspire-5750G.(none)>
2013-08-08 14:11:16 +0100

    display movement done

--- display.py
+++ display.py
@@ -6,28 +6,52 @@ f = open('virus.dat')
 
 data = pickle.load(f)
 
-coords = data['coords']
-mvs = data['moves']
-virus = data['virus']
+green = pygame.image.load("images/green.png")
+
+def changeCoords(cs):
+    x, y = cs
+    return ((x * 100) + width/2, (y* 100) + height /8)
+
 
 pygame.init()
 width, height = 1000, 600
 screen = pygame.display.set_mode((width, height),0, 32)
 
+
+coords = data['coords']
+mvs = data['moves']
+virus = data['virus']
+
+coords = map(changeCoords, coords)
+targets = coords[:]
 clock = pygame.time.Clock()
 FPS = 24 #24 frames per second
 i = 0
 node_dict = {}
 
+
+
 health_clr = (56, 48, 225)
 infec_clr = (225, 54, 100)
 
+people = []
 num_of_nodes = len(coords)
-time = 0
 
+screen.fill((225,225,225))
+for i in range(num_of_nodes):
+    x, y = coords[i]
+    people.append(screen.blit(green, (x,y)))
+
+time =1
+#print ('time =' time)
 #while loop allows black screen to stay open and close properly
 while True:
     #PROCESSES
+    if time % FPS == 23:
+        if time in mvs:
+            for person, move in mvs[time]:
+                targets[person-1] = coords[move-1]
+    print pygame.time.get_ticks()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -36,27 +60,24 @@ while True:
     #LOGIC
     #LOGIC
     #DRAW
-    screen.fill((225,225,225))
+    screen.fill((255,255,255))
+    for i, o in enumerate(people[::]):
+        x,y = o.x, o.y
+        target_x, target_y = tuple(targets[i])
+        targetVector_x, targetVector_y = ((target_x - x)/FPS, (target_y - y)/FPS)
+        people.append(screen.blit(green, (x+ targetVector_x, y + targetVector_y)))
+    people = people[num_of_nodes:]
 
-
-    def time_changes():
+    def moves():
         pass
 
 
-    for i in range(num_of_nodes):
-        x, y = coords[i - 1]
-        x = int((x*100) + (width/2))
-        y = int((y*100) + (height/2))
-        circle = pygame.draw.circle(screen, health_clr, (x, y),10)#(x,y), radius, hollow
-        circle
-        i += 1
-        print time
-        if time > 900:
-            screen.fill((225,225,225))
+        #print time
+        #if time > 900:
+        #    screen.fill((225,225,225))
 
 
     time += 1
-
     pygame.display.flip() #makes sure everything is drawn on screen
     #DRAW
 
